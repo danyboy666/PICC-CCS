a19c46f5b5af3164d30fc5ef55a07bdfee6d679a
danyboy666 <dany.ferron@outlook.com>
2021-01-20 18:54:59 -0500

    Add files via upload
    This code is heavily based on PIC CCS ex.serial.c and ex.serial2.c.
    The pic will put data coming from the rs_232 stream (ESP) 
    in usb_cdc_putc() and vice-versa whenever usb_cdc_kbhit() has data it   
    will be dumped in the rs_232 stream (ESP) via fputc().

--- PIC18F2550_PICDEM_USB_UART_BRIDGE/main.c
+++ PIC18F2550_PICDEM_USB_UART_BRIDGE/main.c
@@ -1,44 +1,24 @@
 ///////////////////////////////////////////////////////////////////////////////
 /*****************************************************************************/
 /*                                                                           */
-/*                                                                           */ 
+/* This code is heavily based on PIC CCS ex.serial.c and ex.serial2.c.       */
+/*                                                                           */
+/* The pic will put data coming from the rs_232 stream (ESP)                 */
+/* in usb_cdc_putc() and vice-versa whenever usb_cdc_kbhit() has data it     */
+/* will be dumped in the rs_232 stream (ESP) via fputc().                    */
+/*                                                                             */
 /* Copyright Dany Ferron                                                     */
 /*****************************************************************************/
 /*                                                                           */ 
 /*                                                                           */
 /*                                                                           */
-/* le 21 décembre 2020                                                        */
+/* le 20 janvier 2021                                                        */
 /*                                                                           */
 /*****************************************************************************/
 ///////////////////////////////////////////////////////////////////////////////
-#include <18F2550.h>
+
 // compiled with PCW CCS v 4.104
-// PIDCDEM hex output note: prepend :020000040000FA for PIDCDEM software
-#EXPORT (FILE=.\rls\PIC18F2550_PICDEM_USB_UART_BRIDGE_v1_0.HEX)
-//Bibliothèques utilisés
-#include <main.h> //Header pour le programme.
-
-//#include <.\lib\example.c>
-
-
-#device adc=10
-#fuses HSPLL, NOWDT, PLL3, CPUDIV1, USBDIV, NOVREGEN, PUT, NOMCLR, NOLVP
-//#FUSES BROWNOUT, PUT, STVREN, NODEBUG, NOLVP, NOWRT, NOWRTD, NOWRTB, NOWRTC, NOCPD, NOCPB, NOEBTR, NOEBTRB, NOXINST
-#use delay(clock=48000000)
-//#use rs232(baud=115200,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8,stream=ESP,errors)
-#use rs232(uart1, baud=115200, parity=N, bits=8, stream=ESP,errors)
-//This assumes a 8MHz crystal. Chance 'PLLx' to suit your crystal.
-//Can be 4,8, 12, 16, 20 or 24Mhz.
-//Beyond this other changes are needed.....
-
-//bootloader space
-#build(reset=0x800, interrupt=0x808) 
-#org 0, 0x7ff { }
-
-#include <stdlib.h>
-
-// Includes all USB code and interrupts, as well as the CDC API
-#include <usb_cdc.h>
+#include <main.h> //Header file for this program.
 
 void main(void)
 {
@@ -53,8 +33,7 @@ void main(void)
       {
          usb_task();   
          if (kbhit()) 
-         {
-            
+         {           
             c=fgetc(ESP);
             usb_cdc_putc(c);
          }
--- PIC18F2550_PICDEM_USB_UART_BRIDGE/main.h
+++ PIC18F2550_PICDEM_USB_UART_BRIDGE/main.h
@@ -1,15 +1,27 @@
-//#include <18F4550.h>
+/* main.h */
 
-//Fuses
-//#fuses HSPLL,NOWDT,NOPROTECT,NODEBUG,NOBROWNOUT,USBDIV,PLL5,CPUDIV1,VREGEN,PUT,NOMCLR,NOLVP
-//#use delay(clock=48000000)
-//Réserver les 800 premières case mémoire pour le bootloader. (PICDEM)
-//#build(reset=0x800, interrupt=0x808) 
-//#org 0, 0x7ff { }
+#include <18F2550.h> // Include pic device header file
 
+#device adc=10
+#fuses HSPLL, NOWDT, PLL3, CPUDIV1, USBDIV, NOVREGEN, PUT, NOMCLR, NOLVP // 12Mhz resonator
+#use delay(clock=48000000)
 
-//#use rs232(baud=9600,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8)
+// PIDCDEM hex output note: prepend :020000040000FA for PIDCDEM software
+#EXPORT (FILE=.\rls\PIC18F2550_PICDEM_USB_UART_BRIDGE_v1_0.HEX)
 
+// setup rs232
+#use rs232(uart1, baud=115200, parity=N, bits=8, stream=ESP,errors)
+
+//bootloader space for PICDEM FS USB. Comment this is you're flashing directly.
+#build(reset=0x800, interrupt=0x808) 
+#org 0, 0x7ff { }
+
+//include some libraries
+#include <stdlib.h>
+
+// PIC CCS firmware not provided in this code you need to copy them from the driver folder if this is not compiling.
+// Includes all USB code and interrupts, as well as the CDC API
+#include <usb_cdc.h>
 
 
 
