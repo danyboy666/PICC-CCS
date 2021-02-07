b023363b336cefabc4780a496e82babdb27be46a
danyboy666 <dany.ferron@outlook.com>
2021-02-07 17:00:11 -0500

    Add files via upload

---
+++ PIC18F2550_PICDEM_LED_TEST_PROJECT/main.c
@@ -0,0 +1,53 @@
+///////////////////////////////////////////////////////////////////////////////
+/*****************************************************************************/
+/*                                                                           */
+/* Programme de thermostat interfacé avec un capteur de température          */
+/* intelligent Dallas DS18S20, alarmes pour indiquer les temperatures trop   */
+/* basses et trop élevées et déclanchement de plainte électrique.            */
+/*                                                                           */ 
+/* Copyright Dany Ferron                                                     */
+/*****************************************************************************/
+/*                                                                           */ 
+/*                                                                           */
+/*                                                                           */
+/* le 25 octobre 2020                                                        */
+/*                                                                           */
+/*****************************************************************************/
+///////////////////////////////////////////////////////////////////////////////
+
+// compiled with PCW CCS v 4.104
+// PIDCDEM hex output note: prepend :020000040000FA for PIDCDEM software
+#EXPORT (FILE=.\rls\NEW_PROJECT_v1_0.HEX)
+//Bibliothèques utilisés
+#include <main.h> //Header pour le programme.
+
+//#include <.\lib\exemple.c>
+
+#define LEDB pin_C2 //Active sur niveau bas.
+void setup();
+
+/////////////////////////// Main program begin ///////////////////////////////
+
+void main()
+   {  
+    setup(); 
+      while(TRUE) //Boucle infinie.
+      {
+ //       display_temperature();
+      output_high (LEDB);
+      } // En while loop
+     
+} //Fin du programme principal.
+
+void setup()                     
+   {    
+//    lcd_init(); //Initialisation de l'afficheur.
+//    lcd_gotoxy(1,1); //Mettre le curseur à la position.    
+//    lcd_send_byte(0,1); //Commande pour éffacer l'afficheur.
+   output_low (LEDB);
+   }
+   
+
+   
+   
+
