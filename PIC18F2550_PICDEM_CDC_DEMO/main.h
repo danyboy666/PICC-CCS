211955143a4b93fc66f25d9a55d2a4e1399b1f51
danyboy666 <dany.ferron@outlook.com>
2021-02-22 22:15:13 -0500

    Add files via upload

---
+++ PIC18F2550_PICDEM_CDC_DEMO/main.h
@@ -0,0 +1,15 @@
+//#include <18F4550.h>
+
+//Fuses
+//#fuses HSPLL,NOWDT,NOPROTECT,NODEBUG,NOBROWNOUT,USBDIV,PLL5,CPUDIV1,VREGEN,PUT,NOMCLR,NOLVP
+//#use delay(clock=48000000)
+//Réserver les 800 premières case mémoire pour le bootloader. (PICDEM)
+//#build(reset=0x800, interrupt=0x808) 
+//#org 0, 0x7ff { }
+
+
+//#use rs232(baud=9600,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8)
+
+
+
+
